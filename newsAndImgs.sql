0e84f80a3ec14aeaf2caf6a2f1289b0a78aa3e7c
lw <52997853+LWabc@users.noreply.github.com>
2019-08-27 16:49:59 +0800

    这个是mysql数据库

---
+++ newsAndImgs.sql
@@ -0,0 +1,46 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.5.27 : Database - gw
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`gw` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `gw`;
+
+/*Table structure for table `img` */
+
+DROP TABLE IF EXISTS `img`;
+
+CREATE TABLE `img` (
+  `imgid` int(8) NOT NULL COMMENT '图片编号',
+  `imgurl` varchar(200) NOT NULL COMMENT '图片路径',
+  `imgday` datetime DEFAULT NULL COMMENT '图片新增时间',
+  PRIMARY KEY (`imgid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `news` */
+
+DROP TABLE IF EXISTS `news`;
+
+CREATE TABLE `news` (
+  `nsid` int(10) NOT NULL AUTO_INCREMENT,
+  `nsname` varchar(100) NOT NULL,
+  `nsnr` varchar(1000) NOT NULL,
+  `nsday` datetime NOT NULL,
+  `nsimg` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`nsid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
